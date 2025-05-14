27af7403f6e9eab90e9c8f58a3edac8a73e859f5
Fabiola Acarapi <fabiola29298@gmail.com>
2025-05-14 19:01:58 -0400

    adde groups list page

---
+++ frontend/src/app/(public)/myGroups/components/groupsCardList.tsx
@@ -0,0 +1,49 @@
+ 
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Users, ChevronRight  } from "lucide-react"; 
+import React from "react";
+
+// Define data for members
+const groups = [
+    { name: "Downtown Apartment", people: "3" },
+    { name: "Family",  people: "5" },
+    { name: "Colombia Trip",  people: "3" },
+];
+
+ 
+
+export default function GroupsCardList() {
+    return (
+        <>
+        {groups.map((group) => (
+            <>
+        <Card className="my-5 w-full bg-[#12131A] border border-solid border-[#273345] rounded-[24px] text-white ">
+            <CardContent className="flex flex-row items-center justify-between border-none border-[#273345] px-8 py-0">
+                <div className="flex items-center gap-4">
+                    <div className="rounded-[6px] flex items-center justify-center">
+                        <Users className="w-10 h-10" />
+                    </div>
+                    <CardTitle className="font-paragraph-regular-16-medium ">
+                    {group.name}
+                    </CardTitle>
+                    <div className="bg-[#273345] px-3 py-1 rounded-2xl text-xs">
+                    {group.people} Friends
+                    </div>
+                </div>
+                <div className="flex gap-2">
+                    <Button
+                        variant="outline"
+                        className="rounded-full text-xs border bg-[#191D26] border-[#273345]"
+                    >
+                        More  
+                        <ChevronRight />
+                    </Button> 
+                </div>
+            </CardContent>
+ 
+        </Card></>
+        ))}
+    </>
+    )
+}
---
+++ frontend/src/app/(public)/myGroups/page.tsx
@@ -0,0 +1,27 @@
+ 
+import React from "react";
+import GroupsCardList from "./components/groupsCardList"; 
+ 
+export default function Color() {
+    return (
+        <div className="relative w-full min-h-screen   overflow-hidden  ">
+             
+            {/* Main container */}
+            <div className="flex justify-center items-center px-4 py-4">
+
+                {/* Main content */}
+                <main className="w-full max-w-[824px] bg-[#00000040]/70 border-none rounded-[26px] overflow-hidden p-10 ">
+                    {/* Apartment Card */}
+                    <div className="flex justify-between">
+                    <h3 className="text-2xl text-white">Group List</h3>
+                    <p className="text-[#64D5E4] p-2"> <span> Add new group</span> </p>
+                    </div>
+                    
+                    {/* Expenses Card */}
+                    <GroupsCardList/>
+            
+                </main>
+            </div>
+        </div>
+    );
+}
