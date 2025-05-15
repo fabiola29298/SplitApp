c0c2600256f3cfed9706985b42afe4e16c7db373
Fabiola Acarapi <fabiola29298@gmail.com>
2025-05-14 23:31:03 -0400

    feat: logout button

--- frontend/src/app/(public)/myGroups/page.tsx
+++ frontend/src/app/(public)/myGroups/page.tsx
@@ -1,30 +1,91 @@
- 
+'use client'; 
+import { usePrivy } from '@privy-io/react-auth';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation'; 
+
 import React from "react";
 import GroupsCardList from "./components/groupsCardList"; 
 import Link from 'next/link';
-export default function Color() {
-    return (
-        <div className="relative w-full min-h-screen   overflow-hidden  ">
-             
-            {/* Main container */}
-            <div className="flex justify-center items-center px-4 py-4">
 
-                {/* Main content */}
-                <main className="w-full max-w-[824px] bg-[#00000040]/70 border-none rounded-[26px] overflow-hidden p-10 ">
-                    {/* Apartment Card */}
-                    <div className="flex justify-between">
-                    <h3 className="text-2xl text-white">Group List</h3>
-                    
-                    <Link href={"/createGroup"}>
-                    <p className="text-[#64D5E4] p-2"> <span> Add new group</span> </p>
-                            </Link>
-                    </div>
-                    
-                    {/* Expenses Card */}
-                    <GroupsCardList/>
+
+function LoadingComponent() {
+    return <p className="text-white">Cargando datos del usuario y rol...</p>;
+}
+
+export default function MyGroupsPage() {
+    const { ready, authenticated, user, logout } = usePrivy();
+    const router = useRouter();
+ 
+    const [isLoadingRole, setIsLoadingRole] = useState<boolean>(true);
+
+    useEffect(() => {
+        if (ready && !authenticated) {
+            router.push('/');
+        }
+        if (ready && authenticated && user?.wallet?.address) {
+            setIsLoadingRole(true);
+
             
-                </main>
+        } else if (ready && authenticated && !user?.wallet?.address) {
+            console.warn("Usuario autenticado pero sin dirección de wallet.");
+            
+            setIsLoadingRole(false);
+        }
+    }, [authenticated, router, ready, user?.wallet?.address]);
+    const handleLogout = async () => {
+        await logout();
+        router.push('/');
+    };
+
+    if (!ready || (authenticated && isLoadingRole === null) ) {
+        return <LoadingComponent />;
+    }
+
+    if (!authenticated && ready) {  
+        return (
+            <div>
+                <p>Por favor, inicia sesión para acceder a la Dapp.</p>
+                  
             </div>
-        </div>
-    );
+        );
+    }
+
+    
+    if (authenticated !== null) {
+        return (
+            <>
+                <div className="relative w-full min-h-screen   overflow-hidden  ">
+             
+                    {/* Main container */}
+                    <div className="flex justify-center items-center px-4 py-4">
+
+                        {/* Main content */}
+                        <main className="w-full max-w-[824px] bg-[#00000040]/70 border-none rounded-[26px] overflow-hidden p-10 ">
+                            {/* Card */}
+                            <div className='p-5 text-white'>
+                            <p  >Welcome {user?.wallet?.address} </p>
+                            <button onClick={handleLogout}>Logout</button>
+                
+                            </div>
+                            <div className="flex justify-between">
+                            <h3 className="text-2xl text-white">Group List</h3>
+                    
+                            <Link href={"/createGroup"}>
+                            <p className="text-[#64D5E4] p-2"> <span> Add new group</span> </p>
+                                    </Link>
+                            </div>
+                    
+                            {/* Expenses Card */}
+                            <GroupsCardList/>
+            
+                        </main>
+                    </div>
+                </div>
+            </>
+        );
+    }
+    if (!authenticated) return null;
+
+
+    return <LoadingComponent/>;
 }
